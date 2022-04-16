845a27d83102ac0893c9036ce4d6f939e3a7f12b
Kevin Wither <kvn.wither@gmail.com>
2022-04-16 04:19:37 -0600

    commit tree 9c08fb65b4d039f65846c7d2bbb1aacfb1ab4774

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const test1 = document.getElementById("main")
+test1.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<h1> Kevin is the champion</h1>";
+
